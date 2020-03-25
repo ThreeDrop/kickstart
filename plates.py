1e3f41b75709569d05ec6cb4a93aa2d841c41955
yaoyitong <yitong_yao@pku.edu.cn>
2020-03-25 21:49:50 +0100

    2020 kickstart round A

---
+++ plates.py
@@ -0,0 +1,16 @@
+
+T=int(input())
+for i in range(T):
+    N,K,P=[int(s) for s in input().split(" ")] 
+    beauty=[[int(s) for s in input().split(" ")] for j in range(N)]
+    dp=[[0 for ii in range(P+1)] for jj in range(N)]
+    pk=min(P,K)
+    dp[0][1:pk+1]=[sum(beauty[0][0:k1]) for k1 in range(1,pk+1)]   
+    for nn in range(1,N):  
+        sum_s=[sum(beauty[nn][0:cc]) for cc in range(K+1)]
+        for pp in range(1,P+1):   
+            for xx in range(min(pp,K)+1): 
+                dp[nn][pp]=max(dp[nn][pp],sum_s[xx]+dp[nn-1][pp-xx])
+                
+    print("Case #{}: {}".format(i+1, dp[N-1][P])) 
+	
